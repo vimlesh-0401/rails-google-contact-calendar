702ff1fbf6b33dc814e3e1f0299f56831017e6a5
Boris Paillard <boris.paillard@gmail.com>
2015-02-17 13:45:14 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,15 +37,6 @@ class User < ActiveRecord::Base
   def call_api(url)
     response = open(url)
     JSON.parse(response.read)
-
-    #uri = URI.parse(url)
-
-    #http = Net::HTTP.new(uri.host, uri.port)
-    #http.use_ssl = true
-    #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    #request = Net::HTTP::Get.new(uri.request_uri)
-    #result = http.request(request).body
-    #ActiveSupport::JSON.decode(result)
   end
 
 
